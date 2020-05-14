0f3bfd8518ed4349805b806b4d69e3a1430686dc
github username <github email address>
2020-05-14 01:10:07 +0100

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,24 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+   
+    #Set up three different GET requests
+    #Each gets processed by a different route
+    #Returns a 200 status code if successful
+    get '/' do
+        "Hello World"
+    end
+
+    get '/name' do
+        "My name is __"
+    end
+
+    get '/hometown' do
+        "My hometown is __"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is __"
+    end
+
 end
